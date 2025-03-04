62d6b557a2757566500a54815ab61a0214a9d3e3
blzy12 <bmdasuncion22@src.pshs.edu.ph>
2025-03-04 21:04:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function Message() {
     } else if (message.indexOf("aI")!==-1 || message.indexOf("Ai")!==-1) {
         resultText = 'The message ' + message + " " + 'is tampered with! ';
     } else {
-        resultText = 'The message' + {message} + 'is yet to be encoded!';
+        resultText = 'The message' + message + 'is yet to be encoded!';
     }
     document.getElementById("result").innerText = resultText;
     
